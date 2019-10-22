7b54074359879b963293bc10a241ea57271e6792
Felix Rong <rainmore24@gmail.com>
2019-10-23 09:05:18 +1100

    Enable swagger2 documentation

--- app/src/main/scala/com/rainmore/cms/configs/swagger2/Swagger2Config.scala
+++ app/src/main/scala/com/rainmore/cms/configs/swagger2/Swagger2Config.scala
@@ -3,9 +3,12 @@ package com.rainmore.cms.configs.swagger2
 import com.rainmore.cms.modules.BaseModule
 import org.springframework.context.annotation.Configuration
 import springfox.documentation.builders.{PathSelectors, RequestHandlerSelectors}
+import springfox.documentation.service.{ApiInfo, Contact}
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+import scala.jdk.CollectionConverters._
 
 @Configuration
 @EnableSwagger2
@@ -18,6 +21,17 @@ class Swagger2Config {
             .paths(PathSelectors.regex("/api.*"))
             .paths(PathSelectors.any())
             .build()
+            .apiInfo(apiInfo)
+    }
+
+    private def apiInfo: ApiInfo = {
+        new ApiInfo(
+            "CMS APIs", "", "1.0", "Terms of Service",
+            new Contact("Rainmore", "rainmore.com", ""),
+            "Apache License Version 2.0",
+            "https://www.apache.org/licenses/",
+            Set.empty.asJavaCollection
+        )
     }
 
 }
