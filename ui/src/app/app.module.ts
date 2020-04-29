d9b89c087e2f34dbae0f48c63f51b4d49dc1db53
Felix Rong <rainmore24@gmail.com>
2020-04-29 14:07:47 +1000

    Updated UI configurations

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,19 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule }      from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent }     from './app.component';
+import { BrowserModule }    from '@angular/platform-browser';
+import { NgModule }         from '@angular/core';
+import { AppRoutingModule } from '@app/app-routing.module';
+import { AppComponent }     from '@app/app.component';
+import { LoginComponent }   from '@app/modules/auth';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        LoginComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule
     ],
     providers: [],
-    bootstrap: [AppComponent]
+    bootstrap: [LoginComponent]
 })
 export class AppModule {
 }
---
+++ ui/src/app/modules/auth/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+    constructor() {
+
+    }
+
+    ngOnInit() {
+
+    }
+
+}
