5f514522340fc7bb5d1f4b4b2b4aafd514f3f9f2
Felix Rong <rainmore24@gmail.com>
2021-07-25 17:36:19 +1000

    Upgrade UI to Angular 12.1

--- ui/src/app/app.component.spec.ts
+++ ui/src/app/app.component.spec.ts
@@ -1,35 +1,35 @@
-import { async, TestBed }      from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent }        from './app.component';
+import { AppComponent } from '@app/app.component';
 
 describe('AppComponent', () => {
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            imports: [
-                RouterTestingModule
-            ],
-            declarations: [
-                AppComponent
-            ],
-        }).compileComponents();
-    }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
 
-    it('should create the app', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.componentInstance;
-        expect(app).toBeTruthy();
-    });
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
 
-    it(`should have as title 'ui'`, () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.componentInstance;
-        expect(app.title).toEqual('ui');
-    });
+  it(`should have as title 'ui-new'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('ui-new');
+  });
 
-    it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.nativeElement;
-        expect(compiled.querySelector('.content span').textContent).toContain('ui app is running!');
-    });
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ui-new app is running!');
+  });
 });
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,20 +1,40 @@
-import { BrowserModule }    from '@angular/platform-browser';
 import { NgModule }         from '@angular/core';
-import { AppRoutingModule } from '@app/app-routing.module';
-import { AppComponent }     from '@app/app.component';
-import { LoginComponent }   from '@app/modules/auth';
+import { BrowserModule }    from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule }  from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule }    from '@angular/material/icon';
+import { MatListModule }    from '@angular/material/list';
+import { LayoutModule }     from '@angular/cdk/layout';
+
+import { AppRoutingModule }        from '@app/app-routing.module';
+import { AppComponent }            from '@app/app.component';
+import { LoginComponent }          from '@app/system/auth/login.component';
+import { RolesComponent }          from '@app/modules/users/roles/roles.component';
+import { AccountsComponent }       from '@app/modules/users/accounts/accounts.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent }            from '@app/layout/nav.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        LoginComponent
-    ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule
-    ],
-    providers: [],
-    bootstrap: [LoginComponent]
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RolesComponent,
+    AccountsComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
